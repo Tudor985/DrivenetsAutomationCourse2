1af0af358362ceb6535289fb4bf0bed5b755d9c6
Tudor Segarceanu <tsegarceanu@drivenets.com>
2023-02-20 14:00:14 +0200

    my exercises

---
+++ My exercises.py
@@ -0,0 +1,62 @@
+##string methods examples
+
+
+##which are the string methods?
+
+# print(dir("xx"))
+
+# a = "hello"
+# print(a.capitalize())
+# print(help(a.capitalize))
+# a = "HELLO"
+# print(a.capitalize())
+
+
+# print(help("".center))
+#
+# print("Bogdan".center(50))
+# print("Bogdan".center(50, "!"))
+#
+# text = "tudor"*4
+# print(f"{text}")
+#
+#
+# s = "cat"
+# s2 = s[0], "r"
+# print(s2)
+
+
+
+import requests
+# r = requests.get("https://raw.githubusercontent.com/itb-ie/contest/master/text.txt")
+# print(r.text)
+
+# import requests
+# r = requests.get("https://raw.githubusercontent.com/itb-ie/contest/master/text.txt")
+# print(r.text)
+
+# vowels = "aeiou"
+# code = " abcdefghijklmnoprstuwxyz"
+#
+# text = ""
+#
+# for line in r.text.split(("\n")):
+#     idx = 0
+#     for c in line:
+#         if c in vowels:
+#             idx += 1
+#     text += code[idx]
+# print(text)
+
+
+import math
+
+# x = 4
+# y = math.sqrt(x)
+# print(y)
+
+
+x = 8
+y = (math.sqrt(x))
+print(8 - math.sqrt(y))
+
